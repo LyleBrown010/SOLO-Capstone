2631df7f948118ae55ef7075c6814994aa91f9a1
LyleBrown010 <lylebrown010@gmail.com>
2023-09-06 20:23:19 +0200

    admin begin

--- frontend/src/components/AddUserComp.vue
+++ frontend/src/components/AddUserComp.vue
@@ -23,9 +23,73 @@
 
                     <div>
                         <form class="card">
+                            <div>
+                                <label for="firstName" class="form-label">First Name</label>
+                                <input 
+                                v-model="model.user.firstName"
+                                type="text"
+                                id="firstName"
+                                name="firstName"
+                                class="form-control"
+                                />
+                                <label for="lastName" class="form-label">Last Name</label>
+                                <input 
+                                v-model="model.user.lastName"
+                                type="text"
+                                id="lastName"
+                                name="lastName"
+                                class="form-control"
+                                />
+                                <label for="email" class="form-label">Email</label>
+                                <input 
+                                v-model="model.user.email"
+                                type="text"
+                                id="email"
+                                name="email"
+                                class="form-control"
+                                />
+                                <label for="userProfile" class="form-label">userProfile</label>
+                                <input 
+                                v-model="model.user.userProfile"
+                                type="text"
+                                id="userProfile"
+                                name="userProfile"
+                                class="form-control"
+                                />
+                                <label for="userRole" class="form-label">userRole</label>
+                                <input 
+                                v-model="model.user.userRole"
+                                type="text"
+                                id="userRole"
+                                name="userRole"
+                                class="form-control"
+                                />
+                                <label for="userPassword" class="form-label">userPassword</label>
+                                <input 
+                                v-model="model.user.userPassword"
+                                type="text"
+                                id="userPassword"
+                                name="userPassword"
+                                class="form-control"
+                                />
+                            </div>
                         
                         </form>
                     </div>
+                    <div class="modal-footer">
+          <div class="p-3 text-center">
+            <button
+              type="submit"
+              class="btn m-2 add-btn text-font"
+              @click="addUser()"
+            >
+              Register!
+            </button>
+            <button type="reset" class="btn m-2 clr-btn text-font">
+              Clear Fields
+            </button>
+          </div>
+        </div>
                 </div>
             </div>
         </div>
@@ -37,7 +101,6 @@
 export default {
     data(){
         return{
-            addingUserID: null, 
             model:{
                 user: {
                     firstName: "",
--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,3 +1,157 @@
 <template>
-    <h1>This is the admin page 🤠</h1>
+<div>
+  <h1 class="header">Admin</h1>
+  <div>
+    <h1 class="users">Users</h1>
+    <addUser />
+    <div class="table">
+          <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Surname</th>
+          <th>Password</th>
+          <th>Role</th>
+          <th>Email Address</th>
+          <th>Profile Image</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody v-for="user in users" :key="user.userID">
+        <tr v-if="users">
+          <td>{{ user.userID }}</td>
+          <td>{{ user.firstName }}</td>
+          <td>{{ user.lastName }}</td>
+          <td>{{ user.userPassword }}</td>
+          <td>{{ user.userRole }}</td>
+          <td>{{ user.email }}</td>
+          <td class="image">
+            <img
+              :src="user.userProfile"
+              :alt="user.firstName"
+              loading="lazy"
+              class="img-fluid image"
+            />
+          </td>
+          <td>
+            <updateUser :user="user" /><button
+              class="btn"
+              @click="deleteUser(user.userID)"
+            >
+              Delete
+            </button>
+          </td>
+        </tr>
+        <tr v-else>
+          <Spinner />
+        </tr>
+      </tbody>
+    </table>
+    </div>
+
+    <br><br>
+    <div>
+      <h1 class="products">Products</h1>
+      <addProduct />
+      <div class="table">
+        <table >
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+            <th>Category</th>
+            <th>Artist Name</th>
+            <th>Product Image</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody v-for="product in products" :key="product">
+          <tr v-if="products">
+            <td>{{ product.prodID }}</td>
+            <td>{{ product.productName }}</td>
+            <td>{{ product.productDescription }}</td>
+            <td>{{ product.productPrice }}</td>
+            <td>{{ product.category }}</td>
+            <td>{{ product.artistName }}</td>
+            <td>
+              <img
+                :src="product.prodUrl"
+                :alt="product.prodUrl"
+                class="img-fluid image"
+                loading="lazy"
+              />
+            </td>
+            <td>
+              <updateProduct :product="product" />
+              <button @click="deleteProduct(product.prodID)" class="btn">
+                Delete
+              </button>
+            </td>
+          </tr>
+          <tr v-else>
+            <Spinner />
+          </tr>
+        </tbody>
+      </table>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
+
+<script>
+import Spinner from "../components/SpinnerComp.vue";
+import addProduct from "../components/AddProductComp.vue";
+import addUser from "../components/AddUserComp.vue";
+import updateProduct from "../components/UpdateProductComp.vue";
+import updateUser from "@/components/UpdateUserComp.vue";
+export default {
+  components: {
+    Spinner,
+    addProduct,
+    addUser,
+    updateProduct,
+    updateUser,
+  },
+  computed: {
+    users() {
+      return this.$store.state.users;
+    },
+    products() {
+      return this.$store.state.products;
+    },
+    product() {
+      return this.$store.state.product;
+    },
+    user() {
+      return this.$store.state.user;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("fetchProducts");
+    this.$store.dispatch("fetchUsers");
+  },
+  methods: {
+    deleteProduct(prodID) {
+      if (confirm("Are you sure you want to delete this product?")) {
+        this.$store.dispatch("deleteProduct", prodID);
+        setTimeout(() => {
+          location.reload();
+        }, 500);
+      }
+    },
+    deleteUser(id) {
+      if (confirm("Are you sure you want to delete this user?")) {
+        this.$store.dispatch("deleteUser", id);
+        setTimeout(() => {
+          console.log("Deleting now...");
+          location.reload();
+        }, 500);
+      }
+    },
+  },
+};
+</script>
